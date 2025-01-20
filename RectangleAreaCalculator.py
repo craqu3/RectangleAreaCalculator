638a0cc74fe35ae547ca90a034e181721e7c60c3
Gabriel Rocha <a.rochagabri@gmail.com>
2025-01-20 09:35:05 +0000

    Initial commit

---
+++ RectangleAreaCalculator.py
@@ -0,0 +1,12 @@
+'''
+Name.......:    Gabriel Rocha
+Programmer.:    craqu3(C)
+Observation:    A simple rectangle area calculator
+'''
+
+width = float(input("Insert rectangle width:\n"))
+length = float(input("Insert rectangle length:\n"))
+
+result = float(width * length)
+
+print(f"The result is: {result}")
